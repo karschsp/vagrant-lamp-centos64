07a9e090a19deb5b19f31c23a51829940bcbb136
Steve Karsch <steve@stevekarsch.com>
2013-03-21 14:48:32 -0400

    Fixing typo grap_epel to grab_epel

--- manifests/misc.pp
+++ manifests/misc.pp
@@ -8,7 +8,7 @@ class misc {
     notify  => Service["httpd"]
   }
 
-  exec { "grap-epel":
+  exec { "grab-epel":
     command => "/bin/rpm -Uvh http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm",
     creates => "/etc/yum.repos.d/epel.repo",
     alias   => "grab-epel",
